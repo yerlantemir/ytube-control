f27221aeb6f3e1c3e625256615ef4642e4a6f8be
yerlantemir <temirerlan99@gmail.com>
2023-10-11 12:09:43 +0600

    feat: sync slider value with levels

--- content.js
+++ content.js
@@ -1,26 +1,10 @@
-const css = {
-  hidden: `
-    yt-image.ytd-thumbnail img {
-        visibility: hidden;
-    }
+const PATHNAME_CHECK_INTERVAL_IN_MS = 200;
+const DEFAULT_DISTRACTION_LEVEL = 100;
 
-    #time-status.style-scope.ytd-thumbnail-overlay-time-status-renderer,
-    [is-shorts],
-    a[title="Shorts"],
-    ytd-item-section-renderer#sections[section-identifier="comment-item-section"],
-    #related {
-        display: none;
-    }
-    `,
-};
+const STYLE_TAG = document.createElement("style");
+document.documentElement.appendChild(STYLE_TAG);
 
-const elem = document.createElement("style");
-document.documentElement.appendChild(elem);
-
-const updateElem = async () => {
-  elem.innerHTML = `/* Injected by the Chrome extension */
-  ${css.hidden}`;
-};
+updateElem({ isFirstLoad: true });
 
 let lastPathname = window.location.pathname;
 setInterval(() => {
@@ -28,22 +12,130 @@ setInterval(() => {
     lastPathname = window.location.pathname;
     updateElem();
   }
-}, 200);
+}, PATHNAME_CHECK_INTERVAL_IN_MS);
 
-const callback = function (mutationsList, observer) {
-  for (let mutation of mutationsList) {
-    if (mutation.addedNodes.length) {
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.sliderValue) {
+    updateElem({
+      distractionLevel: message.sliderValue,
+    });
+  }
+});
+
+const distractingElements = {
+  thumbnail: {
+    css: `
+        yt-image.ytd-thumbnail img {
+            visibility: hidden;
+        }
+        `,
+    level: 25,
+  },
+  comments: {
+    css: `
+        ytd-item-section-renderer#sections[section-identifier="comment-item-section"] {
+            display: none;
+        }
+        `,
+    level: 25,
+  },
+  relatedVideos: {
+    css: `
+        #related {
+            display: none;
+        }
+    `,
+    level: 75,
+  },
+  shorts: {
+    css: `
+        [is-shorts],
+        a[title="Shorts"] {
+            display: none;
+        }
+    `,
+    level: 100,
+    show: () => {
+      const element = document.querySelector('[title="Shorts"]');
+      if (element) {
+        element.style.display = "block";
+      }
+    },
+    hide: () => {
       const element = document.querySelector('[title="Shorts"]');
       if (element) {
         element.style.display = "none";
-        observer.disconnect();
       }
-    }
-  }
+    },
+    onFirstLoadCallback: () => {
+      const observeAndHideShortsTab = function (mutationsList, observer) {
+        for (let mutation of mutationsList) {
+          if (mutation.addedNodes.length) {
+            const element = document.querySelector('[title="Shorts"]');
+            if (element) {
+              element.style.display = "none";
+              observer.disconnect();
+            }
+          }
+        }
+      };
+
+      const observer = new MutationObserver(observeAndHideShortsTab);
+
+      observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true,
+      });
+    },
+  },
+  videoDuration: {
+    css: `
+        #time-status.style-scope.ytd-thumbnail-overlay-time-status-renderer {
+            display: none;
+        }
+    `,
+    level: 100,
+  },
 };
 
-const observer = new MutationObserver(callback);
+async function updateElem({ distractionLevel, isFirstLoad = false }) {
+  const storedDistractionLevel = await getStoredDistractionLevel();
 
-observer.observe(document.documentElement, { childList: true, subtree: true });
+  const computedDistractionLevel =
+    distractionLevel ?? storedDistractionLevel ?? DEFAULT_DISTRACTION_LEVEL;
 
-updateElem();
+  let injectingCss = "";
+
+  Object.keys(distractingElements).forEach((key) => {
+    const { css, level, onFirstLoadCallback, hide, show } =
+      distractingElements[key];
+    // inject css
+    if (computedDistractionLevel >= level) {
+      injectingCss += css;
+    }
+    if (
+      isFirstLoad &&
+      onFirstLoadCallback &&
+      computedDistractionLevel >= level
+    ) {
+      onFirstLoadCallback();
+    }
+    if (computedDistractionLevel >= level && hide) {
+      hide();
+    }
+    if (computedDistractionLevel < level && show) {
+      show();
+    }
+  });
+
+  STYLE_TAG.innerHTML = `/* Injected by the Chrome extension */
+      ${injectingCss}`;
+}
+
+async function getStoredDistractionLevel() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("sliderValue", function ({ sliderValue }) {
+      resolve(sliderValue);
+    });
+  });
+}
--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
+const DEFAULT_DISTRACTION_LEVEL = 100;
+
+document.addEventListener("DOMContentLoaded", async function () {
   const sliderInput = document.querySelector('input[type="range"]');
+
+  // Fetch the value from storage and set it to the input and styles
+  const defaultComputedSliderValue =
+    (await getStoredDistractionLevel()) ?? DEFAULT_DISTRACTION_LEVEL;
+
+  sliderInput.value = defaultComputedSliderValue;
+  sliderInput.parentNode.style.setProperty(
+    "--value",
+    defaultComputedSliderValue
+  );
+  sliderInput.parentNode.style.setProperty(
+    "--text-value",
+    JSON.stringify(defaultComputedSliderValue)
+  );
 
   sliderInput.addEventListener("input", function () {
     this.parentNode.style.setProperty("--value", this.value);
@@ -7,5 +23,27 @@ document.addEventListener("DOMContentLoaded", function () {
       "--text-value",
       JSON.stringify(this.value)
     );
+
+    chrome.storage.local.set({
+      sliderValue: this.value,
+    });
+    const sliderValue = this.value;
+    // Get the current active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      let activeTab = tabs[0];
+
+      // Send a message to the active tab (where your content script should be running)
+      chrome.tabs.sendMessage(activeTab.id, {
+        sliderValue,
+      });
+    });
   });
 });
+
+function getStoredDistractionLevel() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("sliderValue", function ({ sliderValue }) {
+      resolve(sliderValue);
+    });
+  });
+}
